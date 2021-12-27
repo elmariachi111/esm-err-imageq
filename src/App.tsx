880337d14cc6fd6619833bd6b8399a256391814d
Stefan <stadolf@gmail.com>
2021-12-27 11:41:24 +0100

    adds imageq

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as iq from 'image-q';
+import ImageToColors from 'image-to-colors';
+import sizeOf from 'image-size';
+
 
 function App() {
+  useEffect(() => {
+    (async() => {
+      const image = "https://ipfs.getsplice.io/ipfs/QmUZ1EUcfHjYvYPhnDXYCHupX72pyUjwVpo1LMZz9EjAkn/cx_12945.png";
+      const pixels = await ImageToColors.getFromExternalSource(image, {
+        setImageCrossOriginToAnonymous: true
+      });
+      const sz = sizeOf(image);
+      const rgb = Uint8Array.from(pixels.flatMap((p) => [...p, 255]));
+      console.log(sz, rgb);
+
+      const pointContainer = iq.utils.PointContainer.fromUint8Array(
+        rgb,
+        sz.width || 100,
+        sz.height || 100,
+      );
+    
+    const distanceCalculator = new iq.distance.EuclideanBT709NoAlpha();
+    const paletteQuantizer = new iq.palette.WuQuant(distanceCalculator, 10);
+    paletteQuantizer.sample(pointContainer);
+    const qPalette = paletteQuantizer.quantizeSync();
+
+    console.log(qPalette.getPointContainer().toUint8Array());
+  })();
+  })
   return (
     <div className="App">
       <header className="App-header">
